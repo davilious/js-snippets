6a0eb3530ef4abb607b3892bc87fab220cf4f5fa
davilious <miguelangel@davilious.com>
2016-05-10 15:05:27 +0200

    Array - Reduce i

---
+++ reduce/reduce-i.js
@@ -0,0 +1,30 @@
+var votes = [
+  "react",
+  "react",
+  "angular",
+  "angular",
+  "react",
+  "ember",
+  "backbone",
+  "vanilla"
+];
+
+var initialValue = {};
+
+//
+// "obj" here is whatever obj return the last time was run
+// "vote" each of the items in array
+//
+var reducer = function(obj, vote) {
+  if (obj[vote] === undefined) {
+    obj[vote] = 1;
+  } else {
+    obj[vote] = obj[vote] + 1;
+  }
+  // important, return the accumulator
+  return obj;
+};
+
+var result = votes.reduce(reducer, initialValue);
+
+console.log(result);
